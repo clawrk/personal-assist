325bb9b72f002d93bd49ac5a8867b6065114ee2c
Gautam <40182371+clawrk@users.noreply.github.com>
2018-07-26 02:05:32 +0530

    Update create_py_files.py

--- create_py_files.py
+++ create_py_files.py
@@ -1,10 +1,34 @@
 import subprocess
 import os
 import sys
-os.chdir("d:")
-string=input("Enter the python file name\n")
-string=string+".py"
-file=open(string,'w')
+
+
+"""
+this code will save a file with proper extension .
+by default the file will be saved inside D drive.
+"""
+
+#input filename and its type
+filename=input("Enter the file name\n")
+ext=input("Enter the extension\n")
+string=filename+"."+ext
+
+
+
+"""
+the variable dir is set to D: drive by default , which must be changed according to the output extracted from the data file
+the data file will give us the folder which will have maximum probability of storing files of that filetype
+"""
+
+
+
+dir="d:"
+os.chdir(dir)
+
+
+
+file=open(string , 'w')
 #if such a fle exists already then it will be overwritten
-file.write("import sys\nimport os\nfrom PyQt5 import QtWidgets,QGui")
+
+file.write("import sys\nimport os\n")
 file.close()
